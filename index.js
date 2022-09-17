aa9db78fe0fb1b8639c851b5d40e106d5cce77e8
asimmakhmudov <asimmahmudov18@gmail.com>
2022-09-17 22:46:23 +0400

    update

--- index.js
+++ index.js
@@ -12,21 +12,23 @@ const cors = require('cors');
 
 dotenv.config();
 app.use(express.json());
-app.use('/images', express.static(path.join(__dirname, '/images')));
+app.use(cors());
 
+// Connect to DB
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useFindAndModify: true,
-    // useCreateIndex: true,
 })
-    .then(
-        () => console.log('MongoDB Connected')
-    )
-    .catch(
-        err => console.log(err)
-    );
+.then(
+    () => console.log('MongoDB Connected')
+)
+.catch(
+    err => console.log(err)
+);
 
+
+// for image upload
+app.use('/images', express.static(path.join(__dirname, '/images')));
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'images');
@@ -34,29 +36,12 @@ const storage = multer.diskStorage({
         cb(null, req.body.name)
     }
 });
-
 const upload = multer({ storage: storage });
-
 app.post("/api/upload", upload.single("file"), (req, res) => {
     res.status(200).json("File has been uploaded");
 })
 
-// NOT WORKING
-// var corsOptions = {
-//     origin: 'https://limonblog.netlify.app',
-//     optionsSuccessStatus: 200,
-//     credentials: true,
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     allowedHeaders: 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept',
-// }
-
-app.use("/api/auth", authRoute, cors(corsOptions));
-app.use("/api/users", usersRoute, cors(corsOptions));
-app.use("/api/posts", postsRoute, cors(corsOptions));
-app.use("/api/categories", categoriesRoute, cors(corsOptions));
-// app.use(cors(corsOptions));
-
-// updated
+// updated, before routes
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -66,6 +51,24 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+
+// routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/categories", categoriesRoute);
+
+
+// NOT WORKING
+// var corsOptions = {
+//     origin: 'https://limonblog.netlify.app',
+//     optionsSuccessStatus: 200,
+//     credentials: true,
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     allowedHeaders: 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept',
+// }
+// app.use(cors(corsOptions));
+
 
 app.listen(process.env.PORT || 8000, () => {
     console.log("Backend is running on port " + process.env.PORT || "8000");
